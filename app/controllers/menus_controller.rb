00e0bfdda545742a57cc327a31f6eb8f9ad3d1af
Misu759 <71998782+Misu759@users.noreply.github.com>
2022-08-07 12:22:55 +0900

    menu追加機能

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,8 +4,27 @@ class MenusController < ApplicationController
     @menus = Menu.all
   end
 
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      flash[:notice] = "Menu was created successfully"
+      redirect_to @menu
+    else
+      render 'new'
+    end
+  end
+
   def show
     @menu = Menu.find(params[:id])
   end
   
+end
+
+private
+def menu_params
+  params.require(:menu).permit(:name, :regular_cost, :description)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :menus, only: %i[index show]
+  resources :menus, only: %i[index show new create]
 end
