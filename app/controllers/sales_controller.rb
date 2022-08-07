f8c32120c462ad32f0c0a89ea6e57b0e23c91ecc
Misu759 <71998782+Misu759@users.noreply.github.com>
2022-08-07 15:53:31 +0900

    売上入力機能を追加

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -7,4 +7,23 @@ class SalesController < ApplicationController
   def show
     @sale = Sale.find(params[:id])
   end
+
+  def new
+    @sale = Sale.new
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+    if @sale.save
+      flash[:notice] = "売上情報の追加完了"
+      redirect_to @sale
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def sale_params
+    params.require(:sale).permit(:quantity, :menu_id, :date)
+  end
 end
