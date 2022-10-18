ce8c6a599d2e995f6ccd2281316c507a73440901
Misu759 <71998782+Misu759@users.noreply.github.com>
2022-10-18 18:54:42 +0900

    サイドバー表示/非表示機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("chartkick")
 require("chart.js")
+require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -20,3 +21,5 @@ require("chart.js")
 import "bootstrap";
 import "chartkick/chart.js";
 import '@fortawesome/fontawesome-free/js/all';
+
+import "./scripts"
