a588e10c05e6f69ebd7f3182c00818d1bd0d707b
Misu759 <71998782+Misu759@users.noreply.github.com>
2022-11-04 22:49:52 +0900

    fix: PostgreSQLに関するエラー修正

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,9 +1,9 @@
 module PurchasesHelper
   def purchase_cost_per_date
-    cost_per_date_columns = @purchases.joins(:ingredient).select("SUM(amount * purchase_cost) AS cost").group(:purchase_date).order(nil)
+    cost_per_date_list = @purchases.joins(:ingredient).unscope(:order).group(:purchase_date).pluck(:purchase_date, "SUM(amount * purchase_cost) AS cost")
     graph_data = []
-    cost_per_date_columns.each do |cost_per_date_column|
-      graph_data << [cost_per_date_column.purchase_date, cost_per_date_column.cost]
+    cost_per_date_list.each do |cost_per_date|
+      graph_data << [cost_per_date[0], cost_per_date[1]]
     end
     graph_data
   end
